f7f81fbdd08803fdbcaa1afd3b208141984ef24b
Roni Gal <gotlib@gmail.com>
2018-07-24 12:24:09 +0200

    prod var

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   logoText: "Lennox Creative",
-  title: "portfolio",
+  titleText: "portfolio",
   footerText: "סטודיו למיתוג גרפי המתמחה ביצירת מותגים אנרגטיים רבי עוצמה החל מרמת הקונספט, דרך בניית השפה המותגית ויישומה במדיות השונות.",
   imagesAmount: 38
 };
