bd4c47a2af99fbd9323afaa6fc448ebc44ebc008
Roni Gal <gotlib@gmail.com>
2018-07-25 18:29:26 +0200

    slide starts on click

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { IsMusicService } from '../../services/is-music.service';
-
+import * as $ from 'jquery';
+import 'bootstrap';
+ 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss']
 })
 export class SliderComponent implements OnInit {
+  @ViewChild('carousel') carousel: ElementRef; 
   public imagesAmount: number;
-  private clickedOnce: boolean;
+  private clickedOnce: boolean; 
 
   constructor(private isMusicService: IsMusicService) {}
 
@@ -26,6 +29,7 @@ export class SliderComponent implements OnInit {
   setMusic(val: boolean) {
     if (!this.clickedOnce) {
       this.isMusicService.setIsMusic(val);
+      $(this.carousel.nativeElement).carousel();
     }
   }
 
